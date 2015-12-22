2e1786e281934f176664fe2dabaf5ed3741189ef
Simon <simon.ternoir@unitag.io>
2015-12-22 14:29:09 +0100

    expand handlebars task file

--- tasks/handlebars.js
+++ tasks/handlebars.js
@@ -1,20 +1,20 @@
 'use strict';
 
-var gulp		= require('gulp');
-var config		= require('./config.json');
-var handlebars = require('gulp-compile-handlebars');
+var gulp				= require('gulp');
+var config				= require('./config.json');
+var handlebars			= require('gulp-compile-handlebars');
 
 
 // Compile handlebars templates
 gulp.task('handlebars', function () {
 	return gulp.src(config.handlebars.src)
 	.pipe(handlebars(
-		{firstName: 'JC'},
-		{batch: ['src/templates/partials']}
-	))
+		{
+			firstName: 'JC'
+		},
+		{
+			batch: ['src/templates/partials']
+		}
+		))
 	.pipe(gulp.dest(config.buildDir));
 });
-
-
-// GLOB POUR POUVOIR UTILISER LES PARTIALS (BATCH) AVEC DES PATHS DIFFÉRENTS!
-// IDEM POUR RENOMER LES FICHIERS EN .HTML !!
